b535d3a8d6df57987e9b38b42ee985a63373322c
Hussnain Ali <hussnainkhilgi1@gmail.com>
2022-09-28 22:23:54 +0200

    ADDED TESTS

--- tests/testthat/test-ApiFunction.R
+++ tests/testthat/test-ApiFunction.R
@@ -7,17 +7,27 @@ test_that("Input check doesnot works for fetchWeatherData()", {
   expect_error(fetchWeatherData(12))
 })
 
-test_that("Output check works for fetchWeatherData()", {
-  # a <- 3
-  # repeat {
-  #   result <- fetchWeatherData("sweden")
-  #   a <- a - 1
-  #   if (a < 1) {
-  #     stop()
-  #   }
-  # }
+
+test_that("Output check doesnot works for fetchWeatherData()", {
   result <- fetchWeatherData("sweden")
   expect_true(is.data.frame(result))
   expect_true(is.data.frame(result))
   expect_equal(colnames(result)[1],"time")
 })
+
+
+test_that("Load check doesnot works for fetchWeatherData()", {
+
+  a <- 10
+  repeat {
+    print(fetchWeatherData("pakistan"))
+    a <- a - 1
+    if (a < 1) {
+       testthat::expect_true(is.data.frame(fetchWeatherData("pakistan")))
+      break;
+    }
+  }
+})
+
+
+
